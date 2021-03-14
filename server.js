d07bb553d3d894e8fd6fd5bbf117570c28183315
Aman Dwivedi <19dwivediaman@gmail.com>
2021-03-14 19:32:18 +0530

    create basic structure

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose")
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
 
+mongoose.connect('mongodb://localhost:27017/UrlDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
 app.get("/", (req,res) => {
-    res.render("index");
+  res.render("index");
 });
 
+app.post("/shortUrls", (req,res) => {
+
+});
 
 app.listen(process.env.PORT || 3000);
