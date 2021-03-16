f10988f8eefcfd1cab398268c53d25f954b6b17b
Aman Dwivedi <19dwivediaman@gmail.com>
2021-03-16 18:45:05 +0530

    commit

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 const express = require("express");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const shortUrl = require("./modals/shortUrl");
+const ShortUrl = require("./modals/shortUrl");
+
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
+app.use(express.urlencoded({extended: true}));
 
-mongoose.connect('mongodb://localhost:27017/UrlDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/urlShortner', {useNewUrlParser: true, useUnifiedTopology: true});
 
 
-app.get("/", (req,res) => {
-  res.render("index");
+app.get("/", async (req,res) => {
+  const shortUrls = await ShortUrl.find()
+	res.render("index",{shortUrls: shortUrls});
 });
 
-app.post("/shortUrls", (req,res) => {
-
+app.post("/shortUrls", async (req,res) => {
+	shortUrl.create({fill: req.body.fullUrl})
+	res.redirect("/");
 });
+
+app.get("/:shortUrl", async (req,res) => {
+	const shortUrl = await ShortUrl.findOne({short: req.params.shortUrl })
+	if(shortUrl == null)
+		return res.sendStatus(404);
+	
+	shortUrl.clicks++;
+	ShortUrl.save();
+
+	res.redirect(shortUrl.full);
+})
 
 app.listen(process.env.PORT || 3000);
